92b73c59fb5baf4a4e8ce49a261f647b3fc62e9d
Jeffrey Bergier <jeffburg@jeffburg.com>
2025-07-27 18:16:42 +0900

    [bootstrap] added document type

---
+++ ChatDocs/Controller/Sources/Controller/Document.swift
@@ -0,0 +1,49 @@
+//
+// GPLv3 License Notice
+//
+// Copyright (c) 2025 Jeffrey Bergier
+//
+// This file is part of ChatDocs.
+// ChatDocs is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+// ChatDocs is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with ChatDocs. If not, see <https://www.gnu.org/licenses/>.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+import Model
+
+public struct Document: FileDocument {
+  
+  public static let readableContentTypes = [
+    UTType(importedAs: "com.saturdayapps.chatdocs.document")
+  ]
+  
+  public var model: DocumentModel
+  
+  public init(model: DocumentModel = .init()) {
+    self.model = model
+  }
+  
+  public init(configuration: ReadConfiguration) throws {
+    let plistDecoder = PropertyListDecoder()
+    guard let data = configuration.file.regularFileContents else {
+      throw CocoaError(.fileReadCorruptFile)
+    }
+    let model = try plistDecoder.decode(DocumentModel.self, from: data)
+    self.model = model
+  }
+  
+  public func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+    let plistEncoder = PropertyListEncoder()
+    let data = try plistEncoder.encode(self.model)
+    return .init(regularFileWithContents: data)
+  }
+}
